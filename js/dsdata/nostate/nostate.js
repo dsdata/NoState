ae3a62770cf8534cae0674a12fb09688e4c6055e
Daniel Sasse <info@golox-web.de>
2011-12-22 12:08:31 +0100

    Bugfix Release - Version 1.1.5 - Dec 22, 2011

--- js/dsdata/nostate/nostate.js
+++ js/dsdata/nostate/nostate.js
@@ -29,7 +29,7 @@
  * @package Dsdata_NoState
  * @authors Daniel Sasse <info@golox-web.de, http://www.golox-web.de/>
  * @developer Daniel Sasse <info@golox-web.de, http://www.golox-web.de/> 
- * @version 1.1.4
+ * @version 1.1.5
  * @copyright Golox-Web.de, Daniel Sasse
  * @license http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)  
  */
